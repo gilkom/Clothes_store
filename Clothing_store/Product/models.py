7d84adfa20c2e0a014d2f4cdb4faf5324e1f3900
gko <marcingilko@gmail.com>
2022-01-06 13:36:38 +0100

    add products, manufacturers and categories

--- Clothing_store/Product/models.py
+++ Clothing_store/Product/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Manufacturer(models.Model):
+    def __str__(self):
+        return self.name
+
+    name = models.CharField(max_length=60)
+    description = models.TextField(blank=True)
+
+
+class Category(models.Model):
+    def __str__(self):
+        return self.name
+
+    name = models.CharField(max_length=30)
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+
+class Product(models.Model):
+    def __str__(self):
+        return self.name
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=12, decimal_places=2)
--- Clothing_store/Product/views.py
+++ Clothing_store/Product/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def index(request):
+    query = Product.objects.all()
+    one = Product.objects.get(pk=1)
+    kat = Product.objects.filter(category=1)
+    return HttpResponse(kat)
