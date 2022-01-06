ae8a3e19159b16b1a7e9b1daa480f05d259127c9
gko <marcingilko@gmail.com>
2022-01-06 15:09:52 +0100

    add index html

--- Clothing_store/Clothing_store/urls.py
+++ Clothing_store/Clothing_store/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Product.views import index
+from Product.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index)
+    path('', index),
+    path('category/<id>', category, name='category')
 ]
--- Clothing_store/Product/views.py
+++ Clothing_store/Product/views.py
@@ -1,9 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Category
+
 
 def index(request):
     query = Product.objects.all()
     one = Product.objects.get(pk=1)
     kat = Product.objects.filter(category=1)
-    return HttpResponse(kat)
+    man = Product.objects.filter(manufacturer=2)
+    cat_name = Category.objects.get(id=4)
+    categories = Product.objects.filter(category__isnull=False)
+    contains = Product.objects.filter(description__icontains='bla')
+    data = {'categories': categories}
+    return render(request, 'indek.html', data)
+
+
+def category(request, id):
+    category_user = Category.objects.get(pk=id)
+    return HttpResponse(category_user)
