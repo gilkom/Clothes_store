51760df72c3e1ef7cae6b9f6e7d2b36d57b9e040
gko <marcingilko@gmail.com>
2022-01-11 19:56:12 +0100

    add templates

--- Clothing_store/Product/views.py
+++ Clothing_store/Product/views.py
@@ -8,8 +8,8 @@ def index(request):
     one = Product.objects.get(pk=1)
     kat = Product.objects.filter(category=1)
     man = Product.objects.filter(manufacturer=2)
-    cat_name = Category.objects.get(id=4)
-    categories = Product.objects.filter(category__isnull=False)
+    categories = Category.objects.all()
+    # categories = Product.objects.filter(category__isnull=False)
     contains = Product.objects.filter(description__icontains='bla')
     data = {'categories': categories}
     return render(request, 'indek.html', data)
@@ -17,4 +17,14 @@ def index(request):
 
 def category(request, id):
     category_user = Category.objects.get(pk=id)
-    return HttpResponse(category_user)
+    category_product = Product.objects.filter(category = category_user)
+    categories = Category.objects.all()
+    data = {'category_user': category_user, 'category_product': category_product, 'categories': categories}
+    return render(request, 'category_product.html', data)
+
+
+def product(request, id):
+    product_user = Product.objects.get(pk=id)
+    categories = Category.objects.all()
+    data = {'product_user': product_user, 'categories': categories }
+    return render(request, 'product.html', data)
